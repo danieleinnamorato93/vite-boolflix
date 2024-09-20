951ffc1d8e4c9eab2538c64c9a1deaebe12d7cc7
danieleinnamorato93 <daniele.innamorato93@gmail.com>
2024-09-20 16:28:57 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+import AppMain from './components/AppMain.vue';
+export default {
+  data() {
+    return {
+      
+    }
+  },
+  components:{
+   AppMain,
+  }
+}
+</script>
+
+<template>
+<AppMain/>
+</template>
+
+<style lang="scss">
+@use "../src/style/general.scss"
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,20 @@
+<script>
+import AppSearch from './AppSearch.vue';
+export default {
+  data() {
+    return {
+      
+    }
+  },
+  components:{
+    AppSearch,
+  }
+}
+</script>
+
+<template>
+<AppSearch/>
+</template>
+
+<style scoped>
+</style>
