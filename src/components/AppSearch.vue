cb88d25072cf15c6a7d63e8d865c6505e7eb119b
danieleinnamorato93 <daniele.innamorato93@gmail.com>
2024-09-20 16:41:57 +0200

    searchbar and button created

--- src/components/AppSearch.vue
+++ src/components/AppSearch.vue
@@ -10,7 +10,25 @@ export default {
 
 <template>
 <h2>AppSearch</h2>
+<div class="searchbar">
+  <input type="text" placeholder="Cerca...">
+  <button type="submit">INVIA</button>
+</div>
 </template>
 
 <style lang="scss" scoped>
+  .search-bar {
+      position: relative;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      border-radius: 25px;
+      overflow: hidden;
+    }
+    button {
+      padding: 1px;
+      background-color: black;
+      color: red;
+      font-weight: bold;
+    }
 </style>
